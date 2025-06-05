aa91c06f0ffdd53b03148ce556fbc6e66705dbd2
emiiFlorees <A100229089@my.uvm.edu.mx>
2025-06-05 08:41:41 -0600

    Update somos.js

--- somos.js
+++ somos.js
@@ -1,3 +1,3 @@
 document.getElementById("btnPresentacion").addEventListener("click", function() {
-    window.open("https://www.canva.com/presentacion-id", "_blank");
+    window.open("https://www.canva.com/design/DAGpfUrACZM/NCTy4b52mdn0FnRU2q9Pnw/edit?utm_content=DAGpfUrACZM&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton", "_blank");
 });
